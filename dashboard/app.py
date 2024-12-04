2ac3bb1f04304bed7af2c335c154ae0bcfd9b2ef
Deadeye111 <leandro01@bluewin.ch>
2024-12-04 12:53:49 +0100

    new app update for sf models

--- dashboard/app.py
+++ dashboard/app.py
@@ -97,6 +97,38 @@ def arima_predictions(dist_id, offense, n_periods):
         if ROUND_PREDICTIONS:
             pickle_preds = pickle_preds.round()
         return pickle_preds
+    
+def sf_predictions(dist_id, offense, n_periods):
+    model_store_path = f"../dataAnalysis/sf_models_statsforecast/{dist_id}/{offense}"
+    
+    if os.path.exists(model_store_path):
+        with open(os.path.join(model_store_path, 'sf_model.pkl'), 'rb') as pkl:
+            sf_model = pickle.load(pkl)
+        
+        test_df = load_crime_data(dist_id, offense, subset="test")
+        # Rename columns
+        test_df = test_df.rename(columns={"Reported_Date": "ds", "Crime_Count": "y"})
+        # Add unique_id column
+        test_df['unique_id'] = dist_id
+        # Ensure the date column is in datetime format
+        test_df['ds'] = pd.to_datetime(test_df['ds'])
+
+        n_periods = min(n_periods, len(test_df))
+        n_periods -= 1
+
+        # Ensure n_periods is positive
+        if n_periods <= 0:
+            raise ValueError("n_periods must be greater than 0 after adjustment.")
+
+        # Generate predictions by doing cross validation
+        forecast = sf_model.cross_validation(
+            df=test_df.head(n_periods+1),
+            h=1, # Horizon is 1, since we forecast the next day
+            step_size=1, # Move the window by 1 time step
+            n_windows=n_periods
+        )
+
+        return forecast
 
 def create_sequences(data, time_steps=1):
     X, y = [], []
@@ -162,7 +194,7 @@ def lstm_predictions(dist_id, offense, n_periods):
         return predictions
 
 # Function to create and display the plot
-def display_predictions(dist_id, offense, date_range, n_periods, predictions):
+def display_predictions_lstm(dist_id, offense, date_range, n_periods, predictions):
     st.subheader(f"Prediction Results for {n_periods} next days:")
     
     # Generate a date range for the predictions
@@ -182,11 +214,11 @@ def display_predictions(dist_id, offense, date_range, n_periods, predictions):
     # Plotting
     fig, ax = plt.subplots(figsize=(10, 6))
 
+    # Plot test data
+    ax.plot(test_df['Reported_Date'], test_df['Crime_Count'], label="Test Data", color='grey', linewidth=2)
+
     # Plot predictions
     ax.plot(predictions_df['Reported_Date'], predictions_df['Crime_Count'], label="Predictions", color='blue')
-
-    # Plot test data
-    ax.plot(test_df['Reported_Date'], test_df['Crime_Count'], label="Test Data", color='orange')
 
     # Title and labels
     ax.set_title(f"Predictions vs Test Data for {offense} in District {dist_id}")
@@ -202,6 +234,40 @@ def display_predictions(dist_id, offense, date_range, n_periods, predictions):
     # Adjust layout and display the plot
     plt.tight_layout()
     st.pyplot(fig)
+
+def display_predictions_sf_models(dist_id, offense, date_range, n_periods, predictions_df):
+    st.subheader(f"Prediction Results for {n_periods} next days using Multiple Models:")
+    
+    # Load test data
+    test_df = load_crime_data(dist_id=dist_id, offense=offense, subset="test")
+    test_df['Reported_Date'] = pd.to_datetime(test_df['Reported_Date'])
+    
+    # Plotting
+    fig, ax = plt.subplots(figsize=(12, 8))
+    
+    # Plot predictions for each model
+    models = ['AutoARIMA', 'CrostonOptimized', 'ADIDA', 'IMAPA', 'TSB', 'SESOpt']
+    for model in models:
+        ax.plot(predictions_df['ds'], predictions_df[model], label=f"{model} Predictions")
+    
+    # Plot test data
+    ax.plot(test_df['Reported_Date'], test_df['Crime_Count'], linestyle="dashed", label="Test Data", color='grey', linewidth=1)
+    
+    # Title and labels
+    ax.set_title(f"Predictions vs Test Data for {offense} in District {dist_id}")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Crime Count")
+    
+    # Add legend
+    ax.legend()
+    
+    # Add grid
+    ax.grid()
+    
+    # Adjust layout and display the plot
+    plt.tight_layout()
+    st.pyplot(fig)
+
 
 # Load data
 offenses = load_offenses()
@@ -283,7 +349,7 @@ with subcol2:
         "Select the end date:",
         value=dec_31_24,  # Default end date
         min_value=jan_1_24,  # Ensure the end date cannot be earlier than the start date
-        max_value=dec_31_25,  # Maximum allowed end date
+        max_value=dec_31_24,  # Maximum allowed end date
         format="DD.MM.YYYY",
     )
 
@@ -300,7 +366,7 @@ with subcol2:
 
     st.markdown("---")
     st.subheader("Select the prediction model:")
-    model = st.selectbox("Select the prediction model:", ["ARIMA", "LSTM"])
+    model = st.selectbox("Select the prediction model:", ["Statistical Models", "LSTM"])
 
 st.markdown("---")
 
@@ -312,12 +378,16 @@ if st.button("Run prediction..", use_container_width=True):
         dist_id = f"{district[0]}.0"
         n_periods = (date_range[1] - date_range[0]).days
         
+        plot_cols1, plot_cols2, plot_cols3 = st.columns([1,6,1])
         # Add spinner while running predictions
         with st.spinner(f"Running {model} predictions..."):
-            if model == "ARIMA":
-                predictions = arima_predictions(dist_id, offense, n_periods)
+            if model == "Statistical Models":
+                predictions = sf_predictions(dist_id, offense, n_periods)
+                with plot_cols2:
+                    # Display the predictions for sf models
+                    display_predictions_sf_models(dist_id, offense, date_range, n_periods, predictions)
             else:
                 predictions = lstm_predictions(dist_id, offense, n_periods)
-
-            # Display the predictions once the spinner finishes
-            display_predictions(dist_id, offense, date_range, n_periods, predictions)
+                with plot_cols2:
+                    # Display the predictions for lstm
+                    display_predictions_lstm(dist_id, offense, date_range, n_periods, predictions)
